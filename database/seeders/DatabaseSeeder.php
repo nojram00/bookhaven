ac82962c97236e1940ae37dfcfc24688f7634fa7
nojram00 <petmalu.marjon@gmail.com>
2024-10-31 02:58:36 +0800

    runnnnn

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,54 +16,54 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        //     'password' => bcrypt('password123'),
-        //     'role' => 'admin'
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => bcrypt('password123'),
+            'role' => 'admin'
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'userme',
-        //     'email' => 'userme@example.com',
-        //     'password' => bcrypt('user123'),
-        //     'role' => 'user'
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'userme',
+            'email' => 'userme@example.com',
+            'password' => bcrypt('user123'),
+            'role' => 'user'
+        ]);
 
-        // Book::factory()->create([
-        //     'book_name' => 'Anne of Green Gables',
-        //     'genre' => 'childrens_book',
-        //     'author' => 'Lucy Montgomery',
-        //     'book_overview' => " ",
-        //     'price' => '2000.00',
-        //     'year_published' => '1908'
-        // ]);
+        Book::factory()->create([
+            'book_name' => 'Anne of Green Gables',
+            'genre' => 'childrens_book',
+            'author' => 'Lucy Montgomery',
+            'book_overview' => " ",
+            'price' => '2000.00',
+            'year_published' => '1908'
+        ]);
 
-        // Book::factory()->create([
-        //     'book_name' => 'Ang mga Kaibigan ni Mama Susan',
-        //     'genre' => 'mystery_thriller',
-        //     'author' => 'Bob Ong',
-        //     'book_overview' => " ",
-        //     'price' => '1000.00',
-        //     'year_published' => '2010'
-        // ]);
+        Book::factory()->create([
+            'book_name' => 'Ang mga Kaibigan ni Mama Susan',
+            'genre' => 'mystery_thriller',
+            'author' => 'Bob Ong',
+            'book_overview' => " ",
+            'price' => '1000.00',
+            'year_published' => '2010'
+        ]);
 
-        // Book::factory()->create([
-        //     'book_name' => 'Kapitan Sino',
-        //     'genre' => 'fiction',
-        //     'author' => 'Bob Ong',
-        //     'book_overview' => " ",
-        //     'price' => '1000.00',
-        //     'year_published' => '2009'
-        // ]);
+        Book::factory()->create([
+            'book_name' => 'Kapitan Sino',
+            'genre' => 'fiction',
+            'author' => 'Bob Ong',
+            'book_overview' => " ",
+            'price' => '1000.00',
+            'year_published' => '2009'
+        ]);
 
-        // Book::factory()->create([
-        //     'book_name' => 'Dekada 70',
-        //     'genre' => 'political',
-        //     'author' => 'Lualhati Bautista',
-        //     'book_overview' => " ",
-        //     'price' => '1500.00',
-        //     'year_published' => '1983'
-        // ]);
+        Book::factory()->create([
+            'book_name' => 'Dekada 70',
+            'genre' => 'political',
+            'author' => 'Lualhati Bautista',
+            'book_overview' => " ",
+            'price' => '1500.00',
+            'year_published' => '1983'
+        ]);
     }
 }
